c53dda344f76c5b3b93d43677bac0262d289792f
Leonardo Souza <leonardomibsp@hotmail.com>
2022-03-21 21:51:06 -0300

    submit about page

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+
+export default function about() {
+  return (
+    <div>  
+    <h1>About page</h1>
+    <Link href={'./'}>Voltar</Link>
+    </div>
+
+    
+  )
+  
+
+
+}
