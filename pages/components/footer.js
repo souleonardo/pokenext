70e37fdabeacc0be6dba4b92600e305ff1aece2f
Leonardo Souza <leonardomibsp@hotmail.com>
2022-03-21 23:54:51 -0300

    Add navbar and footer component

---
+++ pages/components/footer.js
@@ -0,0 +1,7 @@
+import Link from 'next/link'
+
+export default function Footer() {
+    return(
+        <p>Next App by Leonardo &copy; 2022 </p>
+    )
+}
--- pages/components/main.js
+++ pages/components/main.js
@@ -1,11 +1,13 @@
 
 import Navbar from "./navbar"
+import Footer from "./footer"
 
 export default function MainContainer({children}){
     return (
         <>
             <Navbar />
             <div>{children}</div>
+            <Footer />
         </> 
     )
     
